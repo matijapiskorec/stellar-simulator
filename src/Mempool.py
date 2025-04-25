92744ad1e9d0a843f0e814e6ab1beae0fcec4f47
jaimedvw <145909282+jaimedvw@users.noreply.github.com>
2025-04-25 13:17:08 +0200

    Push code that nominates all transactions a Node has in its mempool at once, as opposed to one at a time

--- src/Mempool.py
+++ src/Mempool.py
@@ -52,14 +52,18 @@ class Mempool():
         # transaction_mined = Transaction()
         # transaction_mined = Transaction(time=self.simulation_time)
         transaction_mined = Transaction(time=Globals.simulation_time)
-        log.mempool.info('Transaction %s mined to the mempool!', transaction_mined)
-        self.transactions.append(transaction_mined)
-        if not os.path.exists(self.log_path):
-            with open(self.log_path, 'w') as log_file:
-                log_file.write("")
+        if transaction_mined not in self.transactions:
+            log.mempool.info('Transaction %s mined to the mempool!', transaction_mined)
+            self.transactions.append(transaction_mined)
+            if not os.path.exists(self.log_path):
+                with open(self.log_path, 'w') as log_file:
+                    log_file.write("")
 
-        self.log_mine_to_file(f"MEMPOOL - INFO - Transaction {transaction_mined} mined to the mempool!")
-        return transaction_mined
+            self.log_mine_to_file(f"MEMPOOL - INFO - Transaction {transaction_mined} mined to the mempool!")
+            return transaction_mined
+        else:
+            log.mempool.info('Transaction %s could not be mined to the mempool!', transaction_mined)
+
 
     def get_transaction(self):
 
@@ -75,6 +79,14 @@ class Mempool():
             transaction = None
             log.mempool.info('No transactions in the mempool!')
         return transaction
+
+    def get_all_transactions(self):
+        if len(self.transactions) > 0:
+            log.mempool.info('All transactions retrieved from the mempool: %s', self.transactions)
+            return self.transactions.copy()  # Return a copy to avoid external mutation
+        else:
+            log.mempool.info('No transactions in the mempool!')
+            return []
 
     # def update_time(self,simulation_time):
     #     """
